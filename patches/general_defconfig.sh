2ee73ac4ebb11fad72f87f2908619e007fcf0078
zakoxako <102341238+DataEraserC@users.noreply.github.com>
2023-12-10 21:32:07 +0000

    Add patches

---
+++ patches/general_defconfig.sh
@@ -0,0 +1,1 @@
+find arch/arm64/configs/vendor/ -type f -name "*_defconfig" -exec sh -c 'export defconfig_path={} && sed "s@<device_defconfig>@$defconfig_path@g" patches/general_defconfig.patch |patch -p1 ' \;
