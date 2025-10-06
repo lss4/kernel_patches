73c29d0bffe601f1eb0f6bdcdd8cf2aec8c15a84
L.S.S. <30771484+lss4@users.noreply.github.com>
2025-10-07 02:43:42 +0900

    Temp undo removal of DEFAULT_KSU_MNT_ID_FOR_KSU_PROC_UNSHARE.
    It's being used by fs/namespace.c at the moment.

--- patches/4.19.157/susfs-1.5.11/include/linux/susfs_def.h
+++ patches/4.19.157/susfs-1.5.11/include/linux/susfs_def.h
@@ -41,6 +41,7 @@
 #define SUS_SU_WITH_HOOKS 2
 
 #define DEFAULT_KSU_MNT_ID 300000 /* used by mount->mnt_id */
+#define DEFAULT_KSU_MNT_ID_FOR_KSU_PROC_UNSHARE 1000000 /* used by vfsmount->susfs_mnt_id_backup */
 #define DEFAULT_KSU_MNT_GROUP_ID 3000 /* used by mount->mnt_group_id */
 
 /*
